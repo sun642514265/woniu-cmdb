26fd151ad02b8307b9d51fcdc42f97daa3acb681
woniuppp <i@shengxinjing.cn>
2015-12-22 07:36:26 +0800

    bug fix

--- config.py
+++ config.py
@@ -22,46 +22,52 @@ page_config = {
             "name":'password',
             "title":'密码'
         }]
-    },{
-        "name": 'host',
-        "title": '服务器',
-        "data": [{
-            "name": 'cabinet',
-            "title": '机柜'
-        },{
-            "name":'hostname',
-            "title":'主机名'
-        }]
-    },{
-        "title": '业务',
-        "sub":[
-            {
-                'name': 'product',
-                'title': '业务线',
-                'data': [{
-                    'name': 'service_name',
-                    'title': '服务名'
-                },{
-                    'name':'module_letter',
-                    'title':'模块简称'
-                },{
-                    'name':'dev_interface',
-                    'title':'开发者'
-                },{
-                    'name':'op_interface',
-                    'title':'运维接口人'
-                }]
-            },
-            {
-                'name': 'raidtype',
-                'title': 'Raid厂商',
-                'data': [{
-                    'name': 'name',
-                    'title': 'Raid厂商'
-                }]
-            }
-
-
-        ]
     }]
 }
+
+
+
+
+
+# ,{
+#         "name": 'host',
+#         "title": '服务器',
+#         "data": [{
+#             "name": 'cabinet',
+#             "title": '机柜'
+#         },{
+#             "name":'hostname',
+#             "title":'主机名'
+#         }]
+#     },{
+#         "title": '业务',
+#         "sub":[
+#             {
+#                 'name': 'product',
+#                 'title': '业务线',
+#                 'data': [{
+#                     'name': 'service_name',
+#                     'title': '服务名'
+#                 },{
+#                     'name':'module_letter',
+#                     'title':'模块简称'
+#                 },{
+#                     'name':'dev_interface',
+#                     'title':'开发者'
+#                 },{
+#                     'name':'op_interface',
+#                     'title':'运维接口人'
+#                 }]
+#             },
+#             {
+#                 'name': 'raidtype',
+#                 'title': 'Raid厂商',
+#                 'data': [{
+#                     'name': 'name',
+#                     'title': 'Raid厂商'
+#                 }]
+#             }
+
+
+#         ]
+#     }
