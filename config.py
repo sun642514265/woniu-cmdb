c70973d1df736e25d9fbad2789b663b9511b493d
shengxinjing <316783812@qq.com>
2015-12-21 10:56:12 +0800

    bug fix

--- config.py
+++ config.py
@@ -10,19 +10,45 @@ db_config = {
 }
 
 
-# 页面配置
-page_config = [
-    {
-        "name": 'role',
-        "title": '权限',
+page_config = {
+    brand_name:'Woniu Admin'
+    menu:[{
+        "name": 'user',
+        "title": '用户',
+        "desc":'这个是用户的配置，尽量不要改',
         "data": [{
-            "name": 'name',
-            "title": '权限名'
+            "name": 'username',
+            "title": '用户名'
         },{
             "name":'password',
-            "title":'999'
+            "title":'密码'
         }]
-    }
-]
+    },{
+        "name": 'host',
+        "title": '服务器',
+        "data": [{
+            "name": 'cabinet',
+            "title": '机柜'
+        },{
+            "name":'hostname',
+            "title":'主机名'
+        }]
+    }]
+}
+
+# 页面配置
+# page_config = [
+#     {
+#         "name": 'role',
+#         "title": '权限',
+#         "data": [{
+#             "name": 'name',
+#             "title": '权限名'
+#         },{
+#             "name":'password',
+#             "title":'999'
+#         }]
+#     }
+# ]
 
 
